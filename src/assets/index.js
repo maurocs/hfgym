5aae06b626ad7bb81e630851dfead7e7fcc41dc3
Mauro Conde <maurocondes@gmail.com>
2024-03-13 20:57:39 +0000

    cleanup

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,23 +1,9 @@
 import hfLogo from './hfLogo.svg'
 import menu from './menu.svg'
 import close from './close.svg'
-import staffIcon from './staffIcon.png'
-import bikeIcon from './bikeIcon.png'
-import dumbellIcon from './dumbellIcon.png'
-import balloonsIcon from './balloonsIcon.png'
-import bodyIcon from './bodyIcon.png'
-import cycling from './cycling.jpg'
-import HIIT from './HIIT.jpg'
 
 export {
     hfLogo,
     menu,
-    close,
-    staffIcon,
-    bikeIcon,
-    dumbellIcon,
-    balloonsIcon,
-    bodyIcon, 
-    cycling,
-    HIIT
+    close
 }
